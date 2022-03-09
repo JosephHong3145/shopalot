d6dd2018ec569e39ef2a9c0ccd774c6755941212
Armia471 <97773003+Armia471@users.noreply.github.com>
2022-03-09 18:59:05 -0500

    Update OrderConfirmationView.jsx

--- src/components/views/OrderConfirmationView.jsx
+++ src/components/views/OrderConfirmationView.jsx
@@ -1,4 +1,12 @@
-import { Box, Button, Container, Spacing, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  Spacing,
+  Typography,
+} from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import React from "react";
 
@@ -6,19 +14,26 @@ export const OrderConfirmationView = () => {
   return (
     <Container>
       <Typography>OrderConfirmationView</Typography>
-      <Box textAlign="left" sx={{ p: 1 }}>
-        <Typography variant="h5" color="primary">
-          {" "}
-          <CheckCircleIcon color="primary" />
-          Your order is placed, thanks.{" "}
-        </Typography>
-      </Box>
-      <Box marginLeft={4}>
-        <Typography variant="h12">
-          Confirmation will be sent to your email.
-        </Typography>
-      </Box>
-      <Box textAlign="right" marginTop={10}>
+      <Paper elevation={5}>
+        <Box textAlign="left" sx={{ p: 1 }}>
+          <Typography variant="h5" color="primary">
+            {" "}
+            <CheckCircleIcon color="primary" />
+            Your order is placed, thanks.{" "}
+          </Typography>
+        </Box>
+        <Box marginLeft={4}>
+          <Typography variant="h12" marginBottom={20}>
+            Confirmation will be sent to your email.
+          </Typography>
+        </Box>
+        <Box marginLeft={3}>
+          <Button size="h10" marginLeft={3}>
+            {"Review your orders"}
+          </Button>
+        </Box>
+      </Paper>
+      <Box textAlign="right" marginTop={20}>
         <Button variant="contained" size="small">
           {"Continue shopping"}
         </Button>
