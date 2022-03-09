7ad2189809c9ce0b990483ac52f9a9b861138d88
Armia471 <97773003+Armia471@users.noreply.github.com>
2022-03-09 17:29:45 -0500

    Update OrderConfirmationView.jsx

--- src/components/views/OrderConfirmationView.jsx
+++ src/components/views/OrderConfirmationView.jsx
@@ -1,10 +1,19 @@
+import { Button, Container, Grid, Typography } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import React from "react";
 
-import { Container, Typography } from "@mui/material";
-
 export const OrderConfirmationView = () => {
-  return;
-  <Container>
-    <Typography>OrderConfirmationView</Typography>
-  </Container>;
+  return (
+    <Container>
+      <Typography variant="h5" color="primary">
+        OrderConfirmationView
+      </Typography>
+      <Typography>
+        {" "}
+        <CheckCircleIcon color="primary" />
+        Your order is placed, thanks.
+      </Typography>
+      <Button variant="contained">{"Continue shopping"}</Button>
+    </Container>
+  );
 };
