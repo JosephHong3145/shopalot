5c807477dc83e890032ab7a87366f927d9c418cd
JosephHong3145 <dot7201@gmail.com>
2022-03-18 00:35:03 -0400

    Commit to MyProfile user story
    Commit to MyProfile user story, bonus story for sprint 3. As a user, I want to be able to view all my basic information, as well as all the item that I am selling in the case that I am a vendor. Furthermore, I want to be able to edit said basic information and have them saved and presented.

--- src/components/views/MyProfileView.jsx
+++ src/components/views/MyProfileView.jsx
@@ -8,6 +8,10 @@ import {
   Typography,
 } from "@mui/material";
 
+// TODO:
+// Send and retrieve data to database
+// enclose mechanics in form and make onSubmit function
+
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
@@ -68,192 +72,202 @@ export const MyProfileView = () => {
     console.log("Edit mode: " + isEditMode);
   };
 
-  return (
-    <Container maxWidth="md">
-      <Box mt={4}>
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <Controller
-              name="imageURL"
-              render={({
-                field: { onChange, value },
-                fieldState: { error },
-              }) => (
-                <Box
-                  component="img"
-                  sx={{ width: 1, borderRadius: 2 }}
-                  src={value}
-                />
-              )}
-            />
-          </Grid>
-          <Grid item xs={7}>
-            <Paper variant="outlined" sx={{ height: 1 }}>
-              <Box mt={1} mb={1} mr={1} ml={1}>
-                <Box mt={1} mb={1} display="flex" flexdirection="column">
-                  <Typography variant="h4">
-                    <b>{user.displayName}</b>
-                  </Typography>
-                </Box>
-                <Box mt={1} mb={1} align="bottom">
-                  <FormControl>
-                    <FormLabel id="UserType">User Type:</FormLabel>
-                    <RadioGroup
-                      row
-                      aria-labelledby="UserType"
-                      name="changeUserType"
-                    >
-                      <FormControlLabel
-                        value="Vendor"
-                        control={<Radio />}
-                        label="Vendor"
-                      />
-                      <FormControlLabel
-                        value="Customer"
-                        control={<Radio />}
-                        label="Customer"
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </Box>
-                <Box mt={1} mb={1} display="flex" flexdirection="column">
-                  <Controller
-                    name="phone"
-                    control={control}
-                    rules={{
-                      required: "Phone number required. ",
-                    }}
-                    render={({
-                      field: { onChange, value },
-                      fieldState: { error },
-                    }) => (
-                      <TextField
-                        fullWidth
-                        label="Phone"
-                        variant="outlined"
-                        value={value}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                        onChange={onChange}
-                        disabled={!isEditMode}
-                      />
-                    )}
-                  />
-                </Box>
-                <Box mt={1} mb={1} display="flex" flexdirection="column">
-                  <Controller
-                    name="email"
-                    control={control}
-                    rules={{
-                      required: "Email required.",
-                    }}
-                    render={({
-                      field: { onChange, value },
-                      fieldState: { error },
-                    }) => (
-                      <TextField
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        value={value}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                        onChange={onChange}
-                        disabled={!isEditMode}
-                      />
-                    )}
-                  />
-                </Box>
-                <Box mt={1} mb={1} display="flex" flexdirection="column">
-                  <Controller
-                    name="address"
-                    control={control}
-                    rules={{
-                      required: "Address required. ",
-                    }}
-                    render={({
-                      field: { onChange, value },
-                      fieldState: { error },
-                    }) => (
-                      <TextField
-                        fullWidth
-                        label="Address"
-                        variant="outlined"
-                        value={value}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                        onChange={onChange}
-                        disabled={!isEditMode}
-                      />
-                    )}
-                  />
-                </Box>
-                <Controller
-                  name="userDescription"
-                  control={control}
-                  rules={{
-                    required: "User description required. ",
-                  }}
-                  render={({
-                    field: { onChange, value },
-                    fieldState: { error },
-                  }) => (
-                    <TextField
-                      fullWidth
-                      label="User description"
-                      variant="outlined"
-                      value={value}
-                      error={!!error}
-                      helperText={error ? error.message : null}
-                      onChange={onChange}
-                      multiline
-                      maxRows={10}
-                      disabled={!isEditMode}
-                    />
-                  )}
-                />
-              </Box>
-            </Paper>
-          </Grid>
+  const onSubmit = (form) => {};
 
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container maxWidth="md">
+        <Box mt={4}>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <Controller
+                name="imageURL"
+                control={control}
+                render={({
+                  field: { onChange, value },
+                  fieldState: { error },
+                }) => (
+                  <Box
+                    component="img"
+                    sx={{ width: 1, borderRadius: 2 }}
+                    src={value}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={7}>
+              <Paper variant="outlined" sx={{ height: 1 }}>
+                <Box mt={1} mb={1} mr={1} ml={1}>
+                  <Box mt={1} mb={1} display="flex" flexdirection="column">
+                    <Typography variant="h4">
+                      <b>{user.displayName}</b>
+                    </Typography>
+                  </Box>
+                  <Box mt={1} mb={1} align="bottom">
+                    <FormControl>
+                      <FormLabel id="UserType">User Type:</FormLabel>
+                      <RadioGroup
+                        row
+                        aria-labelledby="UserType"
+                        name="changeUserType"
+                      >
+                        <FormControlLabel
+                          value="Vendor"
+                          control={<Radio />}
+                          label="Vendor"
+                        />
+                        <FormControlLabel
+                          value="Customer"
+                          control={<Radio />}
+                          label="Customer"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </Box>
+                  <Box mt={1} mb={1} display="flex" flexdirection="column">
+                    <Controller
+                      name="phone"
+                      control={control}
+                      rules={{
+                        required: "Phone number required. ",
+                      }}
+                      render={({
+                        field: { onChange, value },
+                        fieldState: { error },
+                      }) => (
+                        <TextField
+                          fullWidth
+                          label="Phone"
+                          variant="outlined"
+                          value={value}
+                          error={!!error}
+                          helperText={error ? error.message : null}
+                          onChange={onChange}
+                          disabled={!isEditMode}
+                        />
+                      )}
+                    />
+                  </Box>
+                  <Box mt={1} mb={1} display="flex" flexdirection="column">
+                    <Controller
+                      name="email"
+                      control={control}
+                      rules={{
+                        required: "Email required.",
+                      }}
+                      render={({
+                        field: { onChange, value },
+                        fieldState: { error },
+                      }) => (
+                        <TextField
+                          fullWidth
+                          label="Email"
+                          variant="outlined"
+                          value={value}
+                          error={!!error}
+                          helperText={error ? error.message : null}
+                          onChange={onChange}
+                          disabled={!isEditMode}
+                        />
+                      )}
+                    />
+                  </Box>
+                  <Box mt={1} mb={1} display="flex" flexdirection="column">
+                    <Controller
+                      name="address"
+                      control={control}
+                      rules={{
+                        required: "Address required. ",
+                      }}
+                      render={({
+                        field: { onChange, value },
+                        fieldState: { error },
+                      }) => (
+                        <TextField
+                          fullWidth
+                          label="Address"
+                          variant="outlined"
+                          value={value}
+                          error={!!error}
+                          helperText={error ? error.message : null}
+                          onChange={onChange}
+                          disabled={!isEditMode}
+                        />
+                      )}
+                    />
+                  </Box>
+                  <Controller
+                    name="userDescription"
+                    control={control}
+                    rules={{
+                      required: "User description required. ",
+                    }}
+                    render={({
+                      field: { onChange, value },
+                      fieldState: { error },
+                    }) => (
+                      <TextField
+                        fullWidth
+                        label="User description"
+                        variant="outlined"
+                        value={value}
+                        error={!!error}
+                        helperText={error ? error.message : null}
+                        onChange={onChange}
+                        multiline
+                        maxRows={10}
+                        disabled={!isEditMode}
+                      />
+                    )}
+                  />
+                </Box>
+              </Paper>
+            </Grid>
+
+            <Grid item xs={2}>
+              {isEditMode ? (
+                <Box>
+                  <Box display="flex" flexdirection="column">
+                    <Button
+                      fullWidth
+                      type="submit"
+                      variant="contained"
+                      onClick={saveProfile}
+                    >
+                      Save
+                    </Button>
+                  </Box>
+                  <Box display="flex" flexdirection="column">
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="error"
+                      onClick={cancelSaveProfile}
+                    >
+                      Cancel
+                    </Button>
+                  </Box>
+                </Box>
+              ) : (
+                <Button fullWidth variant="contained" onClick={editProfile}>
+                  Edit
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </Box>
+        <Box display="flex" flexdirection="column">
           <Grid item xs={2}>
             {isEditMode ? (
-              <Box>
-                <Box display="flex" flexdirection="column">
-                  <Button fullWidth variant="contained" onClick={saveProfile}>
-                    Save
-                  </Button>
-                </Box>
-                <Box display="flex" flexdirection="column">
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    color="error"
-                    onClick={cancelSaveProfile}
-                  >
-                    Cancel
-                  </Button>
-                </Box>
-              </Box>
+              <Box></Box>
             ) : (
               <Button fullWidth variant="contained" onClick={editProfile}>
-                Edit
+                Add Item
               </Button>
             )}
           </Grid>
-        </Grid>
-      </Box>
-      <Box display="flex" flexdirection="column">
-        <Grid item xs={2}>
-          {isEditMode ? (
-            <Box></Box>
-          ) : (
-            <Button fullWidth variant="contained" onClick={editProfile}>
-              Add Item
-            </Button>
-          )}
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </form>
   );
 };
