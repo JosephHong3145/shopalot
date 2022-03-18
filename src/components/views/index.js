3a2fe8f22db50bd50dfe102c1539a0caec9ff414
Eve Gagnon <38845939+evgnon@users.noreply.github.com>
2022-03-17 23:37:57 -0400

    Update index.js

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -7,7 +7,6 @@ export { ItemsView } from "./ItemsView";
 export { MyProfileView } from "./MyProfileView";
 export { NotFoundView } from "./NotFoundView";
 export { ProfileView } from "./ProfileView";
-
 export { CheckoutView } from "./paymentviews/CheckoutView";
 export { EditItemView } from "./EditItemView";
 export { MyOrdersView } from "./MyOrdersView";
